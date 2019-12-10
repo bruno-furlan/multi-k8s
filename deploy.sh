f882d93d8c77aa6c9b61ba6b61e983a191fcbb36
Bruno Furlan <bruno.furlan@dpaschoal.com.br>
2019-12-10 11:36:13 -0300

    fix typo again

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 docker build -t brunofurlan/multi-client:latest -t brunofurlan/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t brunofurlan/multi-server:latest -t brunofurlan/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t brunofurlan/multi-worker:latest -t brunofurlan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push brunofurlan/multi-client
-docker push brunofurlan/multi-server
-docker push brunofurlan/multi-server
+
+docker push brunofurlan/multi-client:latest
+docker push brunofurlan/multi-server:latest
+docker push brunofurlan/multi-worker:latest
+
 docker push brunofurlan/multi-client:$SHA
 docker push brunofurlan/multi-server:$SHA
-docker push brunofurlan/multi-server:$SHA
-kubectl appy -f k8s
+docker push brunofurlan/multi-worker:$SHA
+
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=brunofurlan/multi-server:$SHA
 kubectl set image deployments/client-deployment client=brunofurlan/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=brunofurlan/multi-worker:$SHA
